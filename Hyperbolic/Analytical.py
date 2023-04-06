b12c89e99ad2d0079aace3f73af82eceaf93fd1f
sauroncool <ambujiist@gmail.com>
2023-04-06 14:46:01 +0530

    Universal Fix

--- Hyperbolic/Analytical.py
+++ Hyperbolic/Analytical.py
@@ -2,17 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def Analytical(x_values, t, α):  # Analytic Solution
-    # Solution = np.exp(-4 * (x_values - ( 5 + α * t)) ** 2)    With Periodic BCs
-    peak_point = 5 + α * t
-    while peak_point >= 10:
-        peak_point = peak_point - 10
-    peak_point_2 = 2 + α * t
-    while peak_point_2 >= 10:
-        peak_point_2 = peak_point_2 - 10
-    return np.exp(-(x_values - peak_point) ** 2) + np.exp(-(x_values - (peak_point + 10)) ** 2) + np.exp(
-        -30 * (x_values - peak_point_2) ** 2) + np.exp(-30 * (x_values - (peak_point_2 + 10)) ** 2)
-    # return np.exp(-4 * (x_values - peak_point) ** 2)+np.exp(-4 * (x_values - (peak_point+10)) ** 2)
+def Analytical(u, t, α, Δx):  # Analytic Solution
+    v = u.copy()
+    n = len(u)
+    for i in range(n):
+        v[int((i + α * t / Δx) % Nx)] = u[i]
+    return v
 
 
 # Define the grid parameters
@@ -31,19 +26,19 @@ num_time_step = int(sim_time / Δt)  # Number of time steps
 
 # Define the initial condition
 x_values = np.linspace(0, L, Nx)
-# u = np.exp(-4 * (x_values - 5) ** 2)
-u = np.exp(-30 * (x_values - 2) ** 2) + np.exp(-1 * (x_values - 5) ** 2)
+u = np.exp(-4 * (x_values - 5) ** 2)
+
 # Plot the initial condition
 plt.plot(x_values, u, label="Initial Condition")
 
 # Analytical
-plt.plot(x_values, Analytical(x_values, sim_time, α), label=f"After {sim_time} seconds (analytically)")
+plt.plot(x_values, Analytical(u, sim_time, α, Δx), label=f"After {sim_time} seconds (analytically)")
 
 # Define the simulation parameters
 sim_time_2 = 6  # Total simulation time
 num_time_step_2 = int(sim_time_2 / Δt)  # Number of time steps
 # Analytical
-plt.plot(x_values, Analytical(x_values, sim_time + sim_time_2, α),
+plt.plot(x_values, Analytical(u, sim_time + sim_time_2, α, Δx),
          label=f"After {sim_time + sim_time_2} seconds (analytically)")
 
 plt.xlabel("x")
